1defcf7f49580a3113723d12c7836597248b0f1d
Kallen Tu <kallentu@gmail.com>
2021-01-12 21:55:34 -0500

    Adding necessary common types.

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef SEALOX_COMMON_H
+#define SEALOX_COMMON_H
+
+/// Types and constants used throughout the interpreter.
+/// ie. NULL, size_t, bool, uint8_t
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif // SEALOX_COMMON_H
