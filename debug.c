463321e04783c1eab9bc2e5e7c958979d037c0d0
Kallen Tu <kallentu@gmail.com>
2021-02-01 21:14:47 -0500

    Constants op code added.

--- debug.c
+++ debug.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
   return offset + 1;
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+  // Pulls constant index from the byte in the chunk.
+  uint8_t constant = chunk->code[offset+1];
+  printf("%-16s %4d '", name, constant);
+
+  // Grab actual value of the constant to print something readable.
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+
+  // Moves the offset past the current instruction (opcode and operand)
+  return offset + 2;
 }
 
 void disassembleChunk(Chunk* chunk, const char* name) {
@@ -24,6 +38,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   // For each instruction, dispatch to utility function for displaying it.
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
